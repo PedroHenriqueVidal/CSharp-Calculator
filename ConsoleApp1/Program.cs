db96d960adc7e437ffd5e88f405f0700e4fa67df
Pedro Henrique <pvidal123016@gmail.com>
2024-09-02 03:28:46 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+int operador;
+do{ 
+    Console.WriteLine("Escolha entre 1 - Somar, 2 - Subtrair, 3 - Multiplicar, 4 - Dividir, 5 - Resto, 6 - Potência e 0 - Sair");
+    operador = int.Parse(Console.ReadLine());
+    Console.WriteLine("Escolha o primeiro valor");
+    double n1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Escolha o segundo valor");
+    double n2 = double.Parse(Console.ReadLine());
+
+    double resultado;
+    switch (operador)
+    {
+        case 1:
+            resultado = n1 + n2;
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+            break;
+        case 2:
+            resultado = n1 - n2;
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+        break;
+        case 3:
+            resultado = n1 * n2;
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+            break;
+        case 4:
+            if( n1 == 0 || n2 == 0)
+            {
+            Console.WriteLine("Divisão com zero é impossível");
+            } 
+            else
+            {   
+                resultado = n1 / n2;
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
+        }
+            break;
+        case 5:
+            if (n1 == 0 || n2 == 0)
+            {
+                Console.WriteLine("Divisão com zero é impossível");
+            }
+            else
+            {
+                resultado = n1 % n2;
+                Console.WriteLine($"O resultado do resto da divisão é: {resultado}");
+            }
+            break;
+        case 6:
+            resultado = Math.Pow(n1, n2);
+            Console.WriteLine($"O resultado da potenciação é: {resultado}");
+            break;
+        case 0:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Pressione Enter para tentar novamente.");
+            Console.ReadLine();
+            break;
+    }
+}while (operador != 0);
